c5473cf0ca4104c2e03d5a3360716fe210444cb8
GI <az0.mint9@gmail.com>
2018-01-17 22:54:02 +0900

    AbstractEntityを修正

--- src/main/java/jp/example/model/domain/support/Value.java
+++ src/main/java/jp/example/model/domain/support/Value.java
@@ -1,1 +1,1 @@
-package jp.example.model.domain.support;public interface Value {	@Override	int hashCode();	@Override	boolean equals(Object obj);	@Override	String toString();}
+package jp.example.model.domain.support;public interface Value{	@Override	int hashCode();	@Override	boolean equals(Object obj);	@Override	String toString();}
