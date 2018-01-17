a4da52d67fc1bf6f8c4fc153dd2ceda55fc6f6c9
GI <az0.mint9@gmail.com>
2018-01-17 22:50:01 +0900

    AbstractEntityを修正

--- src/main/java/jp/example/model/domain/models/stock/StockEntity.java
+++ src/main/java/jp/example/model/domain/models/stock/StockEntity.java
@@ -1,1 +1,39 @@
-package jp.example.model.domain.models.stock;import jp.example.model.domain.support.entity.*;public final class StockEntity extends AbstractEntity<StockNo, Stock> implements Stock{	final StockNo stockNo;	final StockType stockType;	private StockEntity(StockNo stockNo, StockType stockType)	{		super(stockNo);		this.stockNo = stockNo;		this.stockType = stockType;	}	public static StockEntity of(StockNo stockNo, StockType stockType)	{		return new StockEntity(stockNo, stockType);	}	@Override	public String toString()	{		return "StockEntity{" + "stockNo=" + stockNo + ", stockType=" + stockType + '}';	}	@Override	public StockNo getStockNo()	{		return stockNo;	}	@Override	public StockType getStockType()	{		return stockType;	}}
+package jp.example.model.domain.models.stock;
+
+import jp.example.model.domain.support.entity.*;
+
+public final class StockEntity extends AbstractEntity<StockNo, Stock> implements Stock
+{
+	final StockNo stockNo;
+	final StockType stockType;
+
+	private StockEntity(StockNo stockNo, StockType stockType)
+	{
+		super(stockNo);
+		this.stockNo = stockNo;
+		this.stockType = stockType;
+	}
+
+	public static StockEntity of(StockNo stockNo, StockType stockType)
+	{
+		return new StockEntity(stockNo, stockType);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "StockEntity{" + "stockNo=" + stockNo + ", stockType=" + stockType + '}';
+	}
+
+	@Override
+	public StockNo getStockNo()
+	{
+		return stockNo;
+	}
+
+	@Override
+	public StockType getStockType()
+	{
+		return stockType;
+	}
+}
