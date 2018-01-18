387a548960845fde614881c0daa04c7b21b2783b
GI <az0.mint9@gmail.com>
2018-01-18 22:03:59 +0900

    Implement domain support

--- src/main/java/jp/example/model/domain/models/stock/Stock.java
+++ src/main/java/jp/example/model/domain/models/stock/Stock.java
@@ -1,1 +1,1 @@
-package jp.example.model.domain.models.stock;import jp.example.model.domain.support.entity.*;public interface Stock extends Entity<StockNo, Stock> {	StockNo getStockNo();	StockType getStockType();}
+package jp.example.model.domain.models.stock;import jp.example.model.domain.support.entity.*;public interface Stock extends Entity<StockNo, Stock> {}
---
+++ src/main/java/jp/example/model/domain/models/stock/StockRepository.java
@@ -0,0 +1,1 @@
+package jp.example.model.domain.models.stock;import jp.example.model.domain.support.repository.*;public interface StockRepository extends Repository<StockNo, Stock, StockList> {}
