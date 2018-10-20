8accd7932c2c9d4a2b3fe8bda26c517bb5a74eff
azmint <az0.mint9@gmail.com>
2018-10-20 20:37:01 +0900

    [Add]  初回コミット

---
+++ src/main/java/jp/sample/taskmanagement/UserForPublish.java
@@ -0,0 +1,28 @@
+package jp.sample.taskmanagement;
+
+class UserForPublish {
+	private String id;
+	private String name;
+
+	public UserForPublish(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
